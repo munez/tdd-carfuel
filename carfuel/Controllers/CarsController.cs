5924a26dcb5cf95762d8e790ce05ed5f12d19289
Kitsakorn Attchoo <kitsakorn@gmail.com>
2016-04-06 14:30:47 +0700

    Add car detail page
    Add car detail page. User can click link at the car list page to this page.
    
    REF: Fixed #1

--- carfuel/Controllers/CarsController.cs
+++ carfuel/Controllers/CarsController.cs
@@ -8,35 +8,49 @@ using CarFuel.Models;
 using CarFuel.Services;
 using Microsoft.AspNet.Identity;
 
-namespace CarFuel.Controllers {
-  public class CarsController : Controller {
+namespace CarFuel.Controllers
+{
+    public class CarsController : Controller
+    {
 
-    private ICarDb db;
-    private CarService carService;
+        private ICarDb db;
+        private CarService carService;
 
-    public CarsController() {
-      db = new CarDb();
-      carService = new CarService(db);
+        public CarsController()
+        {
+            db = new CarDb();
+            carService = new CarService(db);
+        }
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = new Guid(User.Identity.GetUserId());
+            IEnumerable<Car> cars = carService.GetCarsByMember(userId);
+            return View(cars);
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(Car item)
+        {
+            var userId = new Guid(User.Identity.GetUserId());
+            carService.AddCar(item, userId);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            var userId = new Guid(User.Identity.GetUserId());
+            var c = carService.GetCarsByMember(userId).SingleOrDefault(x => x.Id == id);
+
+            return View(c);
+        }
     }
-
-    [Authorize]
-    public ActionResult Index() {
-      var userId = new Guid(User.Identity.GetUserId());
-      IEnumerable<Car> cars = carService.GetCarsByMember(userId);
-      return View(cars);
-    }
-
-    [Authorize]
-    public ActionResult Create() {
-      return View();
-    }
-
-    [HttpPost]
-    [Authorize]
-    public ActionResult Create(Car item) {
-      var userId = new Guid(User.Identity.GetUserId());
-      carService.AddCar(item, userId);
-      return RedirectToAction("Index");
-    }
-  }
 }
